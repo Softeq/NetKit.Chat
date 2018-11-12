ddd8481b7cdc5c998f16a9bca9d8035520fe7079
Andrey Anfilets <andrey.anfilets@softeq.com>
2018-11-12 17:10:26 +0300

    Add web project

---
+++ Softeq.NetKit.Chat.Web/App/Configuration/AuthenticationsConfiguration.cs
@@ -0,0 +1,21 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+namespace Softeq.NetKit.Chat.Web.App
+{
+    public class AuthenticationsConfiguration
+    {
+        public BearerConfiguration Bearer { get; set; }
+
+        public struct BearerConfiguration
+        {
+            public string ApiSecret { get; set; }
+
+            public string Authority { get; set; }
+
+            public bool RequireHttpsMetadata { get; set; }
+
+            public string ApiName { get; set; }
+        }
+    }
+}
---
+++ Softeq.NetKit.Chat.Web/Common/AttachmentsUtils.cs
@@ -0,0 +1,18 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+namespace Softeq.NetKit.Chat.Web.Common
+{
+    public static class AttachmentsUtils
+    {
+        public static string GetExtentionFromMimeType(string mimeType)
+        {
+            switch (mimeType)
+            {
+                case "image/jpeg": return "jpg";
+                case "image/png": return "png";
+                case "video/mp4": return "mp4";
+                default: return null;
+            }
+        }
+    }
+}
