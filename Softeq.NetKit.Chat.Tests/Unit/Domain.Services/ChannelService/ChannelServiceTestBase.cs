bc0411bc12c0e384a893e77d41abce098b3fd7b0
azhukov <alexei.zhukov@softeq.com>
2018-12-13 16:19:28 +0300

    Create channel async 2/3 tests

--- Softeq.NetKit.Chat.Tests/Unit/Domain.Services/ChannelService/ChannelServiceTestBase.cs
+++ Softeq.NetKit.Chat.Tests/Unit/Domain.Services/ChannelService/ChannelServiceTestBase.cs
@@ -1,24 +1,21 @@
 ﻿// Developed by Softeq Development Corporation
 // http://www.softeq.com
 
-using Microsoft.Extensions.Configuration;
 using Moq;
 using Softeq.NetKit.Chat.Data.Cloud.DataProviders;
 using Softeq.NetKit.Chat.Data.Persistent;
 using Softeq.NetKit.Chat.Data.Persistent.Repositories;
-using Softeq.NetKit.Chat.Domain.Services.Configuration;
 using Softeq.NetKit.Chat.Domain.Services.DomainServices;
 using Softeq.NetKit.Chat.Domain.Services.Mappings;
 using Softeq.NetKit.Chat.Domain.Services.Utility;
 
-namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.MessageServiceTests
+namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.ChannelService
 {
-    public abstract class MessageServiceTestBase
+    public abstract class ChannelServiceTestBase
     {
-        protected const int MessageAttachmentsLimit = 10;
-        protected const int LastMessageReadCount = 20;
+        protected readonly IChannelService _channelService;
 
-        protected readonly IMessageService _messageService;
+        protected readonly Mock<IMemberService> _memberServiceMock = new Mock<IMemberService>(MockBehavior.Strict);
 
         protected readonly Mock<IDateTimeProvider> _dateTimeProviderMock = new Mock<IDateTimeProvider>(MockBehavior.Strict);
 
@@ -32,11 +29,9 @@ namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.MessageServiceTests
         protected readonly Mock<IChannelMemberRepository> _channelMemberRepositoryMock = new Mock<IChannelMemberRepository>(MockBehavior.Strict);
         protected readonly Mock<IAttachmentRepository> _attachmentRepositoryMock = new Mock<IAttachmentRepository>(MockBehavior.Strict);
 
-        protected readonly Mock<IConfiguration> _configurationMock = new Mock<IConfiguration>(MockBehavior.Strict);
-        
-        protected readonly Mock<ICloudAttachmentProvider> _cloudAttachmentProviderMock = new Mock<ICloudAttachmentProvider>(MockBehavior.Strict);
+        protected readonly Mock<ICloudImageProvider> _cloudImageProviderMock = new Mock<ICloudImageProvider>(MockBehavior.Strict);
 
-        protected MessageServiceTestBase()
+        protected ChannelServiceTestBase()
         {
             _unitOfWorkMock.Setup(x => x.ChannelRepository).Returns(_channelRepositoryMock.Object);
             _unitOfWorkMock.Setup(x => x.MemberRepository).Returns(_memberRepositoryMock.Object);
@@ -45,26 +40,17 @@ namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.MessageServiceTests
             _unitOfWorkMock.Setup(x => x.ChannelMemberRepository).Returns(_channelMemberRepositoryMock.Object);
             _unitOfWorkMock.Setup(x => x.AttachmentRepository).Returns(_attachmentRepositoryMock.Object);
 
-            var messageAttachmentsLimitSectionMock = new Mock<IConfigurationSection>();
-            messageAttachmentsLimitSectionMock.Setup(x => x.Value).Returns(MessageAttachmentsLimit.ToString());
-            _configurationMock.Setup(x => x.GetSection("Message:MessageAttachmentsLimit")).Returns(messageAttachmentsLimitSectionMock.Object);
-
-            var lastMessageReadCountSectionMock = new Mock<IConfigurationSection>();
-            lastMessageReadCountSectionMock.Setup(x => x.Value).Returns(LastMessageReadCount.ToString());
-            _configurationMock.Setup(x => x.GetSection("Message:LastMessageReadCount")).Returns(lastMessageReadCountSectionMock.Object);
-
-            var attachmentConfiguration = new MessagesConfiguration(_configurationMock.Object);
-
-            _messageService = new MessageService(
-                _unitOfWorkMock.Object, 
-                _domainModelsMapperMock.Object, 
-                attachmentConfiguration, 
-                _cloudAttachmentProviderMock.Object, 
+            _channelService = new Chat.Domain.Services.DomainServices.ChannelService(
+                _unitOfWorkMock.Object,
+                _domainModelsMapperMock.Object,
+                _memberServiceMock.Object,
+                _cloudImageProviderMock.Object,
                 _dateTimeProviderMock.Object);
         }
 
         protected void VerifyMocks()
         {
+            _memberServiceMock.VerifyAll();
             _dateTimeProviderMock.VerifyAll();
 
             _domainModelsMapperMock.VerifyAll();
@@ -76,7 +62,7 @@ namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.MessageServiceTests
             _channelMemberRepositoryMock.VerifyAll();
             _attachmentRepositoryMock.VerifyAll();
 
-            _cloudAttachmentProviderMock.VerifyAll();
+            _cloudImageProviderMock.VerifyAll();
         }
     }
 }
---
+++ Softeq.NetKit.Chat.Tests/Unit/Domain.Services/ChannelService/CreateChannelAsyncTests.cs
@@ -0,0 +1,110 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Softeq.NetKit.Chat.Domain.DomainModels;
+using Softeq.NetKit.Chat.Domain.Exceptions;
+using Softeq.NetKit.Chat.Domain.TransportModels.Request.Channel;
+using Softeq.NetKit.Chat.Domain.TransportModels.Response.Channel;
+using Xunit;
+
+namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.ChannelService
+{
+    public class CreateChannelAsyncTests : ChannelServiceTestBase
+    {
+        [Fact]
+        public void ShouldThrowIfMemberDoesNotExist()
+        {
+            // Arrange
+            _memberRepositoryMock.Setup(x => x.GetMemberBySaasUserIdAsync(It.IsAny<string>()))
+                .ReturnsAsync((Member)null)
+                .Verifiable();
+
+            var request = new CreateChannelRequest("864EB62D-D833-47FA-8A88-DDBFE76AE6A7", "channel name", ChannelType.Public);
+
+            // Act
+            Func<Task> act = async () => { await _channelService.CreateChannelAsync(request); };
+
+            // Assert
+            act.Should().Throw<NetKitChatNotFoundException>()
+                .And.Message.Should().Be($"Unable to create channel. Member {nameof(request.SaasUserId)}:{request.SaasUserId} not found.");
+
+            VerifyMocks();
+        }
+
+        [Fact]
+        public async Task ShouldCreatePublicChannelWithoutPhoto_AndShouldNotAddAllowedMembers()
+        {
+            // Arrange
+            var request = new CreateChannelRequest("864EB62D-D833-47FA-8A88-DDBFE76AE6A7", "channel name", ChannelType.Public)
+            {
+                AllowedMembers = new List<string> { "A53F4EA4-B5BD-494A-8D5F-4B4B172A25F8" }
+            };
+
+            var member = new Member { Id = new Guid("85B1E28A-3E29-48C5-B85B-1563EEB60742") };
+            _memberRepositoryMock.Setup(x => x.GetMemberBySaasUserIdAsync(It.Is<string>(saasUserId => saasUserId.Equals(request.SaasUserId))))
+                .ReturnsAsync(member)
+                .Verifiable();
+
+            _cloudImageProviderMock.Setup(x => x.CopyImageToDestinationContainerAsync(It.IsAny<string>()))
+                .ReturnsAsync((string)null)
+                .Verifiable();
+
+            var utcNow = DateTimeOffset.UtcNow;
+            _dateTimeProviderMock.Setup(x => x.GetUtcNow())
+                .Returns(utcNow)
+                .Verifiable();
+
+            Channel channelToAdd = null;
+            _channelRepositoryMock.Setup(x => x.AddChannelAsync(It.IsAny<Channel>()))
+                .Callback<Channel>(x => channelToAdd = x)
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            ChannelMember channelMemberToAdd = null;
+            _channelMemberRepositoryMock.Setup(x => x.AddChannelMemberAsync(It.IsAny<ChannelMember>()))
+                .Callback<ChannelMember>(x => channelMemberToAdd = x)
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            _channelRepositoryMock.Setup(x => x.IncrementChannelMembersCount(It.IsAny<Guid>()))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            var addedChannel = new Channel();
+            _channelRepositoryMock.Setup(x => x.GetChannelWithCreatorAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(addedChannel)
+                .Verifiable();
+
+            var channelSummaryResponse = new ChannelSummaryResponse();
+            _domainModelsMapperMock.Setup(x => x.MapToChannelSummaryResponse(
+                    It.Is<Channel>(channel => channel.Equals(addedChannel)),
+                    It.IsAny<ChannelMember>(),
+                    It.IsAny<Message>()))
+                .Returns(channelSummaryResponse)
+                .Verifiable();
+
+            // Act
+            var result = await _channelService.CreateChannelAsync(request);
+
+            // Assert
+            VerifyMocks();
+            channelToAdd.Created.Should().Be(utcNow);
+            channelToAdd.Name.Should().Be(request.Name);
+            channelToAdd.Type.Should().Be(request.Type);
+            channelToAdd.CreatorId.Should().Be(member.Id);
+            channelToAdd.Creator.Should().Be(member);
+            channelToAdd.PhotoUrl.Should().Be(null);
+            channelToAdd.Members.Should().BeEquivalentTo(new List<ChannelMember> {channelMemberToAdd});
+
+            _channelMemberRepositoryMock.Verify(prov => prov.AddChannelMemberAsync(It.IsAny<ChannelMember>()), Times.Once);
+            _channelRepositoryMock.Verify(prov => prov.IncrementChannelMembersCount(It.Is<Guid>(channelId => channelId.Equals(channelToAdd.Id))), Times.Once);
+
+            result.Should().BeEquivalentTo(channelSummaryResponse);
+        }
+    }
+}
