364e38170e323aeaba42fb0c7694fb1b3453aeb1
azhukov <alexei.zhukov@softeq.com>
2018-12-13 17:49:45 +0300

    Covered CreateChannelAsync

--- Softeq.NetKit.Chat.Tests/Unit/Domain.Services/ChannelService/CreateChannelAsyncTests.cs
+++ Softeq.NetKit.Chat.Tests/Unit/Domain.Services/ChannelService/CreateChannelAsyncTests.cs
@@ -106,5 +106,125 @@ namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.ChannelService
 
             result.Should().BeEquivalentTo(channelSummaryResponse);
         }
+
+        [Fact]
+        public async Task ShouldCreatePrivateChannelWithPhoto_AndShouldAddAllowedMembers()
+        {
+            // Arrange
+            var allowedMember = new Member
+            {
+                SaasUserId = "1AB5626B-B311-4862-A0F1-AFD21D9F421B"
+            };
+
+            var request = new CreateChannelRequest("864EB62D-D833-47FA-8A88-DDBFE76AE6A7", "channel name", ChannelType.Private)
+            {
+                AllowedMembers = new List<string> { allowedMember.SaasUserId },
+                Description = "Description",
+                PhotoUrl = "PhotoUrl",
+                WelcomeMessage = "WelcomeMessage"
+            };
+
+            var member = new Member { Id = new Guid("85B1E28A-3E29-48C5-B85B-1563EEB60742") };
+            _memberRepositoryMock.SetupSequence(x => x.GetMemberBySaasUserIdAsync(It.IsAny<string>()))
+                .ReturnsAsync(member)
+                .ReturnsAsync(allowedMember);
+            
+            var cloudPhotoUrl = "cloudPhotoUrl";
+            _cloudImageProviderMock.Setup(x => x.CopyImageToDestinationContainerAsync(It.Is<string>(photoUrl => photoUrl.Equals(request.PhotoUrl))))
+                .ReturnsAsync(cloudPhotoUrl)
+                .Verifiable();
+
+            var utcNow = DateTimeOffset.UtcNow;
+            _dateTimeProviderMock.Setup(x => x.GetUtcNow())
+                .Returns(utcNow)
+                .Verifiable();
+
+            Channel channelToAdd = null;
+            _channelRepositoryMock.Setup(x => x.AddChannelAsync(It.IsAny<Channel>()))
+                .Callback<Channel>(x => channelToAdd = x)
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            var channelMembersToAdd = new List<ChannelMember>();
+            _channelMemberRepositoryMock.Setup(x => x.AddChannelMemberAsync(It.IsAny<ChannelMember>()))
+                .Callback<ChannelMember>(x => channelMembersToAdd.Add(x))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            _channelRepositoryMock.Setup(x => x.IncrementChannelMembersCount(It.IsAny<Guid>()))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+
+            var addedChannel = new Channel();
+            _channelRepositoryMock.Setup(x => x.GetChannelWithCreatorAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(addedChannel)
+                .Verifiable();
+
+            var channelSummaryResponse = new ChannelSummaryResponse();
+            _domainModelsMapperMock.Setup(x => x.MapToChannelSummaryResponse(
+                    It.Is<Channel>(channel => channel.Equals(addedChannel)),
+                    It.IsAny<ChannelMember>(),
+                    It.IsAny<Message>()))
+                .Returns(channelSummaryResponse)
+                .Verifiable();
+
+            // Act
+            var result = await _channelService.CreateChannelAsync(request);
+
+            // Assert
+            VerifyMocks();
+            channelToAdd.Created.Should().Be(utcNow);
+            channelToAdd.Name.Should().Be(request.Name);
+            channelToAdd.Type.Should().Be(request.Type);
+            channelToAdd.CreatorId.Should().Be(member.Id);
+            channelToAdd.Creator.Should().Be(member);
+            channelToAdd.PhotoUrl.Should().Be(cloudPhotoUrl);
+            channelToAdd.Description.Should().Be(request.Description);
+            channelToAdd.WelcomeMessage.Should().Be(request.WelcomeMessage);
+            channelToAdd.Members.Should().BeEquivalentTo(channelMembersToAdd);
+
+            _channelMemberRepositoryMock.Verify(prov => prov.AddChannelMemberAsync(It.IsAny<ChannelMember>()), Times.Exactly(2));
+            _channelRepositoryMock.Verify(prov => prov.IncrementChannelMembersCount(It.Is<Guid>(channelId => channelId.Equals(channelToAdd.Id))), Times.Exactly(2));
+            _memberRepositoryMock.Verify(prov => prov.GetMemberBySaasUserIdAsync(It.IsAny<string>()), Times.Exactly(2));
+
+            result.Should().BeEquivalentTo(channelSummaryResponse);
+        }
+
+        [Fact]
+        public void ShouldThrowIfAllowedMemberNotFound()
+        {
+            // Arrange
+            var allowedMember = new Member
+            {
+                SaasUserId = "1AB5626B-B311-4862-A0F1-AFD21D9F421B"
+            };
+
+            var request = new CreateChannelRequest("864EB62D-D833-47FA-8A88-DDBFE76AE6A7", "channel name", ChannelType.Private)
+            {
+                AllowedMembers = new List<string> { allowedMember.SaasUserId }
+            };
+
+            var member = new Member { Id = new Guid("85B1E28A-3E29-48C5-B85B-1563EEB60742") };
+            _memberRepositoryMock.SetupSequence(x => x.GetMemberBySaasUserIdAsync(It.IsAny<string>()))
+                .ReturnsAsync(member)
+                .ReturnsAsync(null);
+
+            _cloudImageProviderMock.Setup(x => x.CopyImageToDestinationContainerAsync(It.IsAny<string>()))
+                .ReturnsAsync((string)null)
+                .Verifiable();
+
+            _dateTimeProviderMock.Setup(x => x.GetUtcNow())
+                .Returns(DateTimeOffset.UtcNow)
+                .Verifiable();
+
+            // Act
+            Func<Task> act = async () => { await _channelService.CreateChannelAsync(request); };
+
+            // Assert
+            act.Should().Throw<NetKitChatNotFoundException>()
+                .And.Message.Should().Be($"Unable to add member to channel. Member saasUserId:{allowedMember.SaasUserId} not found.");
+
+            VerifyMocks();
+        }
     }
 }
