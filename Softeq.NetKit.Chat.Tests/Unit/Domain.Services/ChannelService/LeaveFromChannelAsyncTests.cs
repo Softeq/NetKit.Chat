de7069522674a15c0cf5d7b1f2c44649d293a390
Gredja <gredja.a@gmail.com>
2018-12-28 13:00:13 +0300

    Add empty class

---
+++ Softeq.NetKit.Chat.Tests/Unit/Domain.Services/ChannelService/LeaveFromChannelAsyncTests.cs
@@ -0,0 +1,20 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Xunit;
+
+namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.ChannelService
+{
+   public class LeaveFromChannelAsyncTests : ChannelServiceTestBase
+    {
+        [Fact]
+        public void NoChannel_ShouldThrowIfChannelDoesNotExist()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
