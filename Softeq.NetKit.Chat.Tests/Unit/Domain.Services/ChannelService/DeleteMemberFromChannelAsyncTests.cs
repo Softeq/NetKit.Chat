b761a7e73342e93d91e68070a362352ae30af76a
Gredja <gredja.a@gmail.com>
2018-12-28 15:51:56 +0300

    Add empty class

---
+++ Softeq.NetKit.Chat.Tests/Unit/Domain.Services/ChannelService/DeleteMemberFromChannelAsyncTests.cs
@@ -0,0 +1,15 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Xunit;
+
+namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.ChannelService
+{
+    public class DeleteMemberFromChannelAsyncTests : ChannelServiceTestBase
+    {
+        [Fact]
+        public void ShouldThrowIfChannelDoesNotExist()
+        {
+        }
+    }
+}
