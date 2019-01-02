6a471d74215efb2bc0ca407edda52a97bb866da4
Gredja <gredja.a@gmail.com>
2019-01-02 14:40:17 +0300

    Tests were created

---
+++ Softeq.NetKit.Chat.Tests/Unit/Domain.Services/MemberServiceTests/GetChannelMembersAsyncTests.cs
@@ -0,0 +1,36 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Moq;
+using Softeq.NetKit.Chat.Domain.Exceptions;
+using Softeq.NetKit.Chat.Tests.Unit.Domain.Services.MemberService;
+using Xunit;
+
+namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.MemberServiceTests
+{
+   public class GetChannelMembersAsyncTests : MemberServiceTestBase
+    {
+        [Fact]
+        public void ShouldThrowIfChannelDoesNotExist()
+        {
+            // Arrange
+            var channelId = new Guid("BE5C68F1-5983-4C08-B57B-FD4EFD7295B8");
+
+            _channelRepositoryMock.Setup(x => x.IsChannelExistsAsync(It.IsAny<Guid>()))
+                .ReturnsAsync(false)
+                .Verifiable();
+
+            // Act
+            Func<Task> act = async () => await _memberService.GetChannelMembersAsync(channelId);
+
+            // Assert
+            act.Should().Throw<NetKitChatNotFoundException>().And.Message.Should()
+                .Be($"Unable to get channel members. Channel {nameof(channelId)}:{channelId} not found.");
+
+            VerifyMocks();
+        }
+    }
+}
---
+++ Softeq.NetKit.Chat.Tests/Unit/Domain.Services/MemberServiceTests/MemberServiceTestBase.cs
@@ -0,0 +1,57 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Moq;
+using Softeq.NetKit.Chat.Data.Persistent;
+using Softeq.NetKit.Chat.Data.Persistent.Repositories;
+using Softeq.NetKit.Chat.Domain.Services.DomainServices;
+using Softeq.NetKit.Chat.Domain.Services.Mappings;
+using Softeq.NetKit.Chat.Domain.Services.Utility;
+
+namespace Softeq.NetKit.Chat.Tests.Unit.Domain.Services.MemberService
+{
+    public class MemberServiceTestBase
+    {
+        protected readonly IMemberService _memberService;
+
+
+        protected readonly Mock<IDateTimeProvider> _dateTimeProviderMock = new Mock<IDateTimeProvider>(MockBehavior.Strict);
+
+        protected readonly Mock<IDomainModelsMapper> _domainModelsMapperMock = new Mock<IDomainModelsMapper>(MockBehavior.Strict);
+
+        protected readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>(MockBehavior.Strict);
+        protected readonly Mock<IChannelRepository> _channelRepositoryMock = new Mock<IChannelRepository>(MockBehavior.Strict);
+        protected readonly Mock<IMemberRepository> _memberRepositoryMock = new Mock<IMemberRepository>(MockBehavior.Strict);
+        protected readonly Mock<IMessageRepository> _messageRepositoryMock = new Mock<IMessageRepository>(MockBehavior.Strict);
+        protected readonly Mock<IForwardMessageRepository> _forwardMessageRepositoryMock = new Mock<IForwardMessageRepository>(MockBehavior.Strict);
+        protected readonly Mock<IChannelMemberRepository> _channelMemberRepositoryMock = new Mock<IChannelMemberRepository>(MockBehavior.Strict);
+        protected readonly Mock<IAttachmentRepository> _attachmentRepositoryMock = new Mock<IAttachmentRepository>(MockBehavior.Strict);
+
+        protected MemberServiceTestBase()
+        {
+            _unitOfWorkMock.Setup(x => x.ChannelRepository).Returns(_channelRepositoryMock.Object);
+            _unitOfWorkMock.Setup(x => x.MemberRepository).Returns(_memberRepositoryMock.Object);
+            _unitOfWorkMock.Setup(x => x.MessageRepository).Returns(_messageRepositoryMock.Object);
+            _unitOfWorkMock.Setup(x => x.ForwardMessageRepository).Returns(_forwardMessageRepositoryMock.Object);
+            _unitOfWorkMock.Setup(x => x.ChannelMemberRepository).Returns(_channelMemberRepositoryMock.Object);
+            _unitOfWorkMock.Setup(x => x.AttachmentRepository).Returns(_attachmentRepositoryMock.Object);
+
+            _memberService = new Chat.Domain.Services.DomainServices.MemberService(
+                _unitOfWorkMock.Object,
+                _domainModelsMapperMock.Object,
+                _dateTimeProviderMock.Object);
+        }
+
+        protected void VerifyMocks()
+        {
+            _dateTimeProviderMock.VerifyAll();
+            _domainModelsMapperMock.VerifyAll();
+            _channelRepositoryMock.VerifyAll();
+            _memberRepositoryMock.VerifyAll();
+            _messageRepositoryMock.VerifyAll();
+            _forwardMessageRepositoryMock.VerifyAll();
+            _channelMemberRepositoryMock.VerifyAll();
+            _attachmentRepositoryMock.VerifyAll();
+        }
+    }
+}
